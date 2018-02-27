c77b7f1f1b4ae4d31b19aa03f2a861db01595eeb
Erik Young <erikyoun355@ed.amdsb.ca>
2018-02-27 17:35:54 -0500

    Add project files.

---
+++ InteractiveCard/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace InteractiveCard
+{
+    partial class OrionCard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OrionCard));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RealKnowledge = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.Black;
+            this.StartButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.StartButton.FlatAppearance.BorderSize = 0;
+            this.StartButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.StartButton.Font = new System.Drawing.Font("Signature of the Ancient ", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.ForeColor = System.Drawing.Color.White;
+            this.StartButton.Location = new System.Drawing.Point(183, 107);
+            this.StartButton.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(300, 116);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Press to learn";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // RealKnowledge
+            // 
+            this.RealKnowledge.BackColor = System.Drawing.Color.Transparent;
+            this.RealKnowledge.Font = new System.Drawing.Font("Olivia", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RealKnowledge.ForeColor = System.Drawing.Color.Ivory;
+            this.RealKnowledge.Location = new System.Drawing.Point(93, 239);
+            this.RealKnowledge.Name = "RealKnowledge";
+            this.RealKnowledge.Size = new System.Drawing.Size(755, 281);
+            this.RealKnowledge.TabIndex = 2;
+            this.RealKnowledge.Text = resources.GetString("RealKnowledge.Text");
+            // 
+            // OrionCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(940, 688);
+            this.Controls.Add(this.RealKnowledge);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartButton);
+            this.Font = new System.Drawing.Font("Brush Script Std", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "OrionCard";
+            this.Text = "Orion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label RealKnowledge;
+    }
+}
+
---
+++ InteractiveCard/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+// Interactive Card - Orion Constellation
+// Erik Young
+// 27 February, 2018
+namespace InteractiveCard
+{
+    public partial class OrionCard : Form
+    {
+        public OrionCard()
+        {
+            InitializeComponent();
+            RealKnowledge.Visible = false;
+        }
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sP1 = new SoundPlayer(Properties.Resources.PRescue);
+            SoundPlayer sP2 = new SoundPlayer(Properties.Resources.ZRescue);  // The sounds of the card, from the game StarCraft.
+            SoundPlayer sP3 = new SoundPlayer(Properties.Resources.TRescue);  // Might be a bit loud.
+            sP1.Play();
+            Thread.Sleep(1500);
+            StartButton.Visible = false;
+
+            Graphics cardG = this.CreateGraphics();
+            Brush redBrush = new SolidBrush(Color.OrangeRed);
+            Brush blueBrush = new SolidBrush(Color.Blue);               // the graphics of the card.
+            Pen whitePen = new Pen(Color.Wheat, 14);
+            Brush textBrush = new SolidBrush(Color.Honeydew);
+            Pen bluePen = new Pen(Color.Blue, 2);
+            Pen redPen = new Pen(Color.OrangeRed, 2);
+
+            cardG.Clear(Color.Black);
+            sP3.Play();
+            cardG.FillEllipse(blueBrush, 392, 41, 6, 6);
+            Thread.Sleep(250);
+
+            cardG.FillEllipse(redBrush, 268, 108, 14, 14);
+            Thread.Sleep(250);
+
+ 
+            cardG.FillEllipse(blueBrush, 457, 130, 8, 8);
+            Thread.Sleep(250);
+
+                                     // These blocks are drawing the stars, with a 250 milli delay
+            cardG.FillEllipse(blueBrush, 418, 295, 6, 6);
+            Thread.Sleep(250);
+
+
+            cardG.FillEllipse(blueBrush, 393, 319, 7, 7);
+            Thread.Sleep(250);
+
+ 
+            cardG.FillEllipse(blueBrush, 364, 337, 7, 7);
+            Thread.Sleep(250);
+
+
+            cardG.FillEllipse(blueBrush, 326, 530, 8, 8);
+            Thread.Sleep(250);
+
+
+            cardG.FillEllipse(blueBrush, 533, 488, 16, 16);
+            Thread.Sleep(500);
+
+            sP2.Play();
+            cardG.DrawLine(redPen, 393, 45, 272, 118);
+            Thread.Sleep(150);
+
+            cardG.DrawLine(bluePen, 396, 45, 460, 133);
+            Thread.Sleep(150);
+
+            cardG.DrawLine(redPen, 270, 113, 458, 135);
+            Thread.Sleep(150);                              // These blocks are drawing the faults/lines inbetween our stars
+
+            cardG.DrawLine(bluePen, 462, 135, 422, 297);
+            cardG.DrawLine(redPen, 275, 118, 367, 340);
+            Thread.Sleep(150);
+
+            cardG.DrawLine(bluePen, 420, 298, 395, 325);
+            cardG.DrawLine(bluePen, 367, 341, 395, 325);
+            Thread.Sleep(150);
+
+            cardG.DrawLine(bluePen, 367, 341, 331, 534);
+            cardG.DrawLine(bluePen, 422, 298, 537, 490);
+            Thread.Sleep(1000);
+
+            cardG.Clear(Color.Black);
+            Thread.Sleep(1000);                 // pops open the knowledge to be imparted upon you.
+            RealKnowledge.Visible = true;
+
+        }
+    }
+}
